4232b5101679ead79563b45a9bb2c0b22ba4584e
Michelot Alexis <amichelot@club-internet.fr>
2018-12-09 21:31:20 +0100

    du coup replique exact de GC

--- App.js
+++ App.js
@@ -1,62 +1,76 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React from "react";
+import { GiftedChat, MessageText } from "react-native-gifted-chat";
+import axios from "axios";
+import { View, Text } from "react-native";
 
-export default class App extends React.Component {
+class App extends React.Component {
   state = {
-    isLoadingComplete: false,
+    messages: []
   };
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+  componentDidMount() {
+    axios
+      .get("http://localhost:2001/messages/5c0d3b4e4ccb172a762877b0")
+      .then(response => {
+        this.setState({ messages: response.data });
+      });
+
+    this.ws = new WebSocket("ws://localhost:3000");
+
+    this.ws.onmessage = e => {
+      const message = JSON.parse(e.data);
+
+      this.setState({
+        messages: GiftedChat.append(this.state.messages, message)
+      });
+    };
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+  onSend(messages = []) {
+    this.ws.send(
+      JSON.stringify({
+        text: messages[0].text,
+        name: "Safi",
+        firstName: "Farid",
+        thread: "5c0d3b4e4ccb172a762877b0"
+      })
+    );
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+  render() {
+    return (
+      <GiftedChat
+        user={{ _id: "5c0d3b4e4ccb172a762877ad" }}
+        renderMessageText={props => {
+          console.log(props.currentMessage);
+          if (
+            props.currentMessage.isRequest === true &&
+            props.currentMessage.thread.bike.user === "5c0d3b4e4ccb172a762877ad" // Est-ce que je suis le propriétaire du vélo ?
+          ) {
+            return (
+              <React.Fragment>
+                <MessageText {...props} />
+                <View>
+                  <Text>Vélo à louer</Text>
+                </View>
+              </React.Fragment>
+            );
+          } else {
+            return (
+              <React.Fragment>
+                <MessageText {...props} />
+              </React.Fragment>
+            );
+          }
+        }}
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+      />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+export default App;
